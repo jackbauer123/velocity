638d2f7f0e22644e9e33000dce5d8567aeef4e95
jackbauer123 <251908410@qq.com>
2017-06-06 16:13:31 +0800

    d

--- test-velocity/src/test/java/com/test/VelocityTest.java
+++ test-velocity/src/test/java/com/test/VelocityTest.java
@@ -1,5 +1,7 @@
 package com.test;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Date;
@@ -7,7 +9,10 @@ import java.util.List;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.junit.Test;
@@ -39,4 +44,37 @@ public class VelocityTest {
 		System.out.println(sw.toString());
 		
 	}
+	
+	@Test
+	public void genateHtml(){
+		try {
+			VelocityEngine ve=new VelocityEngine();
+			ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+			ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());  
+			ve.init();
+			
+            //初始化vm模板
+            Template template=ve.getTemplate("genateHtml.vm","UTF-8");
+            //初始化上下文
+            VelocityContext context=new VelocityContext();
+            //添加数据到上下文中
+            context.put("title", "我的第一个velocity页面");
+            //生成html页面
+            PrintWriter pw=new PrintWriter("f:\\myvelocity.html");
+            template.merge(context, pw);
+            //关闭流
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ResourceNotFoundException e) {
+            e.printStackTrace();
+        } catch (ParseErrorException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+    }
+
+	
 }
